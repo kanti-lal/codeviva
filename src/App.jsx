6bf3a4beae70d1c0451573aba7320952757eddd8
kanti-lal <kanti.ylocation@gmail.com>
2024-03-10 23:26:45 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        This is home page of codeviva
+
+      </div>
+    </>
+  )
+}
+
+export default App
